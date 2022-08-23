101cbdc02966cdf84ce9c8591f587068bbeb2e1b
Guillaume Melenchon <gmelenchon@mbcloud.fr>
2022-08-23 17:25:46 +0200

    Ajout navigation

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../hero.service';
 import { Hero } from '../interfaces/Hero';
 
 @Component({
@@ -8,10 +11,22 @@ import { Hero } from '../interfaces/Hero';
 })
 export class HeroDetailComponent implements OnInit {
 
-  @Input() hero? : Hero
-  constructor() { }
+  hero: Hero | undefined;
+
+  constructor(private route: ActivatedRoute, private heroService: HeroService, private location: Location) { }
 
   ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+        .subscribe(hero => this.hero = hero)
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
